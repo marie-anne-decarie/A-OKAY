9e624d0700a513e114797edd6782fb0e3af8a1f8
marie-anne-decarie <157767089+marie-anne-decarie@users.noreply.github.com>
2025-04-02 18:11:33 -0400

    changed the menu in homepage

---
+++ javaScript/menu.js
@@ -0,0 +1,14 @@
+const hamb = document.querySelector('#open');
+const closer = document.querySelector('#close');
+const menu = document.querySelector('#myNav');
+
+hamb.addEventListener('click', openNav);
+closer.addEventListener('click', closeNav);
+
+function openNav() {
+  menu.style.width = "40vw";
+}
+
+function closeNav() {
+  menu.style.width = "0%";
+}
