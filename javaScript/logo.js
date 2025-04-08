6098a0ae43777c5f939de745f7950f7e0f0b6979
marie-anne-decarie <157767089+marie-anne-decarie@users.noreply.github.com>
2025-04-07 22:53:31 -0400

    worked on logo.js

--- javaScript/logo.js
+++ javaScript/logo.js
@@ -1,19 +1,8 @@
 const mylogo = document.querySelector('#logo');
-const mylogopassword = document.querySelector('#logoPassword');
-const mylogoadmin = document.querySelector('#logoAdmin');
 
 mylogo.addEventListener('click', goHome);
-mylogopassword.addEventListener('click', goHomePassword);
-mylogoadmin.addEventListener('click', goHomeAdmin);
 
 function goHome() {
     window.location.href = "index.html"; // Redirect to new page
 }
 
-function goHomePassword() {
-    window.location.href = "password.html";
-}
-
-function goHomeAdmin() {
-    window.location.href = "admin.html";
-}
